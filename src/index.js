6b894fedc9fac6c220b6f2249ef82b35f9bfb91a
GeorgeGUS <georgegus1@gmail.com>
2019-03-18 23:18:00 +0300

    Fix lint issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-  const username = readlineSync.question("\nMay I have your name? ");
+export default () => {
+  const username = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${username}!`);
 };
