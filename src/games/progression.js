a6f2ea12e22bf11153200362495e42ac115269d7
GeorgeGUS <georgegus1@gmail.com>
2019-03-20 17:26:56 +0300

    Refactor valiable names

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,13 @@
 import { getRandomNum } from '../utils';
 import generateGame from '..';
 
-const progressionLength = 10;
+const progLength = 10;
 
 const generateProgression = () => {
   const startNum = getRandomNum(1, 10);
   const progStep = getRandomNum(1, 10);
   const progression = [];
-  for (let i = 0, num = startNum; i < progressionLength; i += 1, num += progStep) {
+  for (let i = 0, num = startNum; i < progLength; i += 1, num += progStep) {
     progression.push(num);
   }
   return progression;
@@ -16,8 +16,8 @@ const generateProgression = () => {
 const description = 'What number is missing in the progression?';
 const generateData = () => {
   const progression = generateProgression();
-  const questPos = getRandomNum(0, progressionLength - 1);
-  const correctAnswer = progression.splice(questPos, 1, '..');
+  const questionIndex = getRandomNum(0, progLength - 1);
+  const correctAnswer = progression.splice(questionIndex, 1, '..');
   const question = progression.join(' ');
   return { question, correctAnswer };
 };
