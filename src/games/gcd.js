05e2243e58b15471a603ed280eb63ae5fd7cc64e
GeorgeGUS <georgegus1@gmail.com>
2019-03-20 20:35:56 +0300

    Change variable name to simplify adding object property

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -12,10 +12,10 @@ const description = 'Find the greatest common divisor of given numbers.';
 const generateData = () => {
   const numA = getRandomNum();
   const numB = getRandomNum();
-  const gcd = calculateGCD(numA, numB);
+  const correctAnswer = calculateGCD(numA, numB);
   return {
     question: `${numA} ${numB}`,
-    correctAnswer: gcd,
+    correctAnswer,
   };
 };
 
