0e933b260fba09c742fc2b54942d7cb261c1dcdf
GeorgeGUS <georgegus1@gmail.com>
2019-03-20 20:57:40 +0300

    Refactor variable names and for loop

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,14 +1,14 @@
 import { getRandomNum } from '../utils';
 import generateGame from '..';
 
-const progLength = 10;
+const length = 10;
 
 const generateProgression = () => {
-  const startNum = getRandomNum(1, 10);
-  const progStep = getRandomNum(1, 10);
+  const start = getRandomNum(1, 10);
+  const step = getRandomNum(1, 10);
   const progression = [];
-  for (let i = 0, num = startNum; i < progLength; i += 1, num += progStep) {
-    progression.push(num);
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
   }
   return progression;
 };
@@ -16,7 +16,7 @@ const generateProgression = () => {
 const description = 'What number is missing in the progression?';
 const generateData = () => {
   const progression = generateProgression();
-  const questionIndex = getRandomNum(0, progLength - 1);
+  const questionIndex = getRandomNum(0, length - 1);
   const correctAnswer = progression.splice(questionIndex, 1, '..');
   const question = progression.join(' ');
   return { question, correctAnswer };
