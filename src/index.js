5352f2d72e7474fb5b938021b796d18d33002d44
GeorgeGUS <georgegus1@gmail.com>
2019-03-21 16:07:14 +0300

    Remove old checks

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ const numOfQuestions = 3;
 
 export default (description, generateData) => {
   console.log('Welcome to the Brain Games!');
-  if (description) {
-    console.log(description);
-  }
+  console.log(description);
   const username = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${username}!\n`);
-  if (!generateData) {
-    return;
-  }
   for (let i = 0; i < numOfQuestions; i += 1) {
     const { question, correctAnswer } = generateData();
     console.log(`Question: ${question}`);
